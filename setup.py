77e3e200d45c580404b1d6542f59e46cc232a29c
denchik <sotikov@rambler.ru>
2020-11-07 22:23:34 +0300

    - file setup.py created - added minimal information to setup.py file - added TODO comments

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='banana_economy',
+    version='0.01',
+    description='Library for economic calculations',
+    author='Denis Sotikov'
+)
+
+'''
+TODO :
+- added information to setup.py file
+- create a basic package structure
+'''
