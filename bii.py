3cbeccba676b2d696cbb6817cee97c1d47375aa0
denchik <sotikov@rambler.ru>
2020-12-08 21:38:30 +0300

    - added function for differentiated loan payment - added function for annuity loan payment

--- bii.py
+++ bii.py
@@ -9,6 +9,7 @@ def simple_interest(period,
     simple_int = (1 + interest / 100) * period * amount_of_money
     return simple_int
 
+
 def compound_interest(period,
                       replenishment_period,
                       interest,
@@ -24,6 +25,34 @@ def compound_interest(period,
         compound_int = compound_int * (1 + interest / 100) + amount_of_money_for_replenishment
     return compound_int
 
+
+def differentiated_loan_payment(amount_of_money,
+                                period,
+                                interest):
+    # period - the amount of time for which money is invested
+    # interest - the interest rate at which money is invested
+    # amount_of_money - the amount of money invested
+    arr = []
+    mp_cnt = period * 12
+    rest = amount_of_money
+    mp_real = amount_of_money / (period * 12.0)
+    while mp_cnt != 0:
+        mp = mp_real + (rest * interest / 1200)
+        arr.append(round(mp, 2))
+        rest = rest - mp_real
+        mp_cnt = mp_cnt - 1
+    return arr, round(sum(arr), 2)
+
+
+def annuity_loan_payments(amount_of_money,
+                          period,
+                          interest):
+    mp_cnt = period * 12
+    r = interest / 1200.0
+    ak = (r * (1 + r) ** mp_cnt) / (((1 + r) ** mp_cnt) - 1)
+    mp = amount_of_money * ak
+    total = mp * mp_cnt
+    return round(mp, 2), round(total, 2)
 
 # TODO Visit JB Web resources:
 # 11
