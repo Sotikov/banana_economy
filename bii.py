ef5e9e024e35da9f1b17d89a02579b1ee4aba48e
denchik <sotikov@rambler.ru>
2020-12-17 22:28:30 +0300

    - added function for interest_money - the function of calculating simple interest has been remade - readme file remade

--- bii.py
+++ bii.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
+def interest_money(period,
+                   interest,
+                   amount_of_money):
+    # period - the amount of time for which money is invested
+    # interest - the interest rate at which money is invested
+    # amount_of_money - the amount of money invested
+    # interest_money - the amount of money received as a percentage of the money invested
+    interest_money = interest / 100 * period * amount_of_money
+    return interest_money
+
+
 def simple_interest(period,
                     interest,
                     amount_of_money):
     # period - the amount of time for which money is invested
     # interest - the interest rate at which money is invested
     # amount_of_money - the amount of money invested
-    simple_int = (1 + interest / 100) * period * amount_of_money
+    # interest_money - the amount of money received as a percentage of the money invested
+    simple_int = amount_of_money + interest_money(period, interest, amount_of_money)
     return simple_int
 
 
